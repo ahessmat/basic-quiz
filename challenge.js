4c2ec04869e117e5c135b59878037b8a9391cd0d
Asa Hess-Matsumoto <ahessmat@gmail.com>
2019-05-15 22:20:09 -0700

    Updated Readme; infinite questions w/ 'exit', and score display to console

--- challenge.js
+++ challenge.js
@@ -1,81 +1,71 @@
-/////////////////////////////
-// CODING CHALLENGE
-
-
-/*
---- Let's build a fun quiz game in the console! ---
-
-1. Build a function constructor called Question to describe a question. A question should include:
-a) question itself
-b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
-c) correct answer (I would use a number for this)
-
-2. Create a couple of questions using the constructor
-
-3. Store them all inside an array
-
-4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
-
-5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
-
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
-
-7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-
---- Expert level ---
-
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
-
-11. Display the score in the console. Use yet another method for this.
-*/
-
-//STEP 7
-(function () { //invoking this as IIFE
+//invoking this as IIFE so it could be a plugin
+(function () { 
     
-//STEP 1
+//Create questions as objects
 var Question = function(text, options, answer){
     this.text = text;
     this.options = options;
     this.answer = answer;
 }
 
-//STEP 2
+//Pregenerated questions
 var q1 = new Question('Who is the good dog?', ['1) Roman','2) Sadie','3) Spike'], 1);
 var q2 = new Question('What is our streetname?', ['1) Sarah','2) Molly','3) Diana', '4) Virginia'], 3);
 var q3 = new Question('What food does Steph not like?', ['1) Chocolate', '2) Pasta', '3) Fishy fish'], 3);
-var q4 = new Question('Asa loves Steph', ['1) TRUE', '2) FALSE'], 2);
-
-//STEP 3
+var q4 = new Question('Asa loves Steph', ['1) TRUE', '2) FALSE'], 1);
+   
+//Other global variables    
 var quiz = [q1, q2, q3, q4]
+var correctAnswers = 0;
+var keepPlaying = true;
 
-//STEP 4
-function nextQuestion(){
-    return quiz[Math.round(Math.random() + 2)]; //will need to change based on # of questions: ...random() + n-1
+//Function for picking a question from the quiz
+function chooseQuestion(){
+    //scales as new questions are added in the future
+    return quiz[Math.floor(Math.random()*quiz.length)];
 };
 
-var chosenQuestion = nextQuestion();
-console.log(chosenQuestion.text);
-for (var i = 0; i < chosenQuestion.options.length; i++) {
-    console.log(chosenQuestion.options[i]);
-}
-
-//STEP 6
-function isRight() {
-    //STEP 5
-    var userInput = prompt('The correct answer is: ');
-    if (userInput == chosenQuestion.answer) {
-        return 'Correct';
-    } else {
-        return 'FAIL';
+//Push a new question to the player  
+function nextQuestion(){
+    
+    //Pick a new question, then output the question and options in console
+    var chosenQuestion = chooseQuestion();
+    console.log(chosenQuestion.text);
+    for (var i = 0; i < chosenQuestion.options.length; i++) {
+        console.log(chosenQuestion.options[i]);
     }
+    
+    //Allow the player to answer, return the score
+    console.log(isRight());
+    console.log('Your current score is: ' + correctAnswers);
+    console.log('---------------------------');
+    
+    //Determine if player's answer is correct
+    function isRight() {
+        //Prompt the user for an answer
+        var userInput = prompt('The correct answer is: ');
+        //Return that answer is correct
+        //Keep track of how many are correct
+        if (userInput == chosenQuestion.answer) {
+            correctAnswers++;
+            return 'Correct';
+        //Provide an option to terminate the quiz by typing 'exit'
+        } else if (userInput == 'exit') {
+            //terminate
+            keepPlaying = false;
+            return 'Successful exit';
+        //Provide feedback to player if incorrect
+        } else {
+            return 'FAIL';
+        }
+    }  
 }
-console.log(isRight());
 
-
+//Keep playing the game until the player terminates the game
+while (keepPlaying === true) {
+    nextQuestion();
+}
+    
 })(); //closing IIFE
 
 
